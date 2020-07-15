185d74d9a3359faafa247168f62f924163c94e86
tdefriess <tyler.defriess@gmail.com>
2020-07-15 13:03:37 -0400

    Ancestor.py solved in guided project.

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -15,13 +15,15 @@ class Graph:
         self.vertices = {}
 
     def add_vertex(self, vertex_id):
-        self.vertices[vertex_id] = set()
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         self.vertices[v1].add(v2)
 
     def get_neighbors(self, vertex_id):
         return self.vertices[vertex_id]
+
 
     def dfs(self, starting_vertex, destination_vertex):
         stack = Stack()
@@ -44,33 +46,70 @@ class Graph:
                 for neighbor in neighbors:
                     stack.push(neighbor)
 
+def build_graph(ancestors):
+    graph = Graph()
+    for parent, child in ancestors:
+        graph.add_vertex(parent)
+        graph.add_vertex(child)
+        graph.add_edge(child, parent)
+    return graph
 
 def earliest_ancestor(ancestors, starting_node):
-    graph = Graph()
-
-    for ancestor in ancestors:
-        for vertex in ancestor:
-            graph.add_vertex(vertex)
-        graph.add_edge(ancestor[0], ancestor[1])
+    graph = build_graph(ancestors)
 
     stack = Stack()
-    stack.push( starting_node)
 
-    paths = []
-    oldest = -1
-    longest_path = 0
+    visited = set()
+    stack.push([starting_node])
 
-    for vertex in graph.vertices:
-        search_path = graph.dfs(vertex, starting_node)
-        paths = paths + [search_path]
+    longest_path = []
+    eldest = -1
 
-    for path in paths:
-        if path is not None:
-            if len(path) > longest_path:
-                longest_path = len(path)
-                oldest = path[0]
-            elif len(path) == longest_path and len(path) > 0:
-                if path[0] < oldest:
-                    oldest = path[0]
+    while stack.size() > 0:
+        path = stack.pop()
+        current_node = path[-1]
 
-            return oldest
+        if len(path) > len(longest_path) or (len(longest_path) and current_node < eldest):
+            longest_path = path
+            if len(longest_path) > 1:
+                eldest = longest_path[-1]
+
+        if current_node not in visited:
+            visited.add(current_node)
+
+            parents = graph.get_neighbors(current_node)
+
+            for parent in parents:
+                new_path = path + [parent]
+                stack.push(new_path)
+
+    return eldest
+
+
+    # for ancestor in ancestors:
+    #     for vertex in ancestor:
+    #         graph.add_vertex(vertex)
+    #     graph.add_edge(ancestor[0], ancestor[1])
+
+    # stack = Stack()
+    # stack.push( starting_node)
+
+    # paths = []
+    # oldest = -1
+    # longest_path = 0
+
+    # for vertex in graph.vertices:
+    #     search_path = graph.dfs(vertex, starting_node)
+    #     paths = paths + [search_path]
+    #     print(paths)
+
+    # for path in paths:
+    #     if path is not None:
+    #         if len(path) > longest_path and path[0] != starting_node:
+    #             longest_path = len(path)
+    #             oldest = path[0]
+    #         elif len(path) == longest_path and len(path) > 0:
+    #             if path[0] < oldest:
+    #                 oldest = path[0]
+
+    #         return oldest
